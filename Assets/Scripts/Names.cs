f18630b3224f9a20c469895585d6f5b55a235ef1
BerkEncami <48757226+BerkEncami@users.noreply.github.com>
2021-04-16 17:57:52 +0300

    Upload

---
+++ Assets/Scripts/Names.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Names : MonoBehaviour
+{
+    public static string[] BotNames = { "Bot1", "Bot2", "AnyName" };
+}
